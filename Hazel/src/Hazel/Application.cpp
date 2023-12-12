436aa30ef8775a900f85b47b9fa646ee0699e37b
Yaoo-Yaoo <151610360+Yaoo-Yaoo@users.noreply.github.com>
2023-12-12 15:07:22 +0800

    Add TimeStep(DeltaTime)

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,6 +3,8 @@
 
 #include "Hazel/Input.h"
 
+#include <glfw/glfw3.h>
+
 namespace Hazel
 {
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -55,8 +57,12 @@ namespace Hazel
 	{
 		while (m_Running)
 		{
+			float time = (float)glfwGetTime();
+			TimeStep timestep = time - m_LastFrameTime;
+			m_LastFrameTime = time;
+			
 			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate();
+				layer->OnUpdate(timestep);
 
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
